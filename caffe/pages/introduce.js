a63b12b2cb9a8abd42bab23c15944263c50e1082
wooseokoh <allsafe1206@gmail.com>
2021-09-18 15:59:22 +0900

    #1-2 Next.js

--- caffe/pages/introduce.js
+++ caffe/pages/introduce.js
@@ -1,10 +1,18 @@
 import Header from '../components/Header'
+import Head from 'next/head'
 
 export default function Introduce() {
     return (
         <div className="container">
+            <Head>
+                <title>서비스소개 - Caffe : 온라인 커피 주문</title>
+            </Head>
             <Header />
-            <h1>Introduce</h1>
+            <h1 className="font-bold">Introduce</h1>
+
+            <p>
+                Caffe 서비스를 이용해서, 온라인으로 주문하고 편하게 현장에서 수령하세요!
+            </p>
         </div >
     )
 }
