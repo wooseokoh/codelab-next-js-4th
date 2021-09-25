3d906b554d0f245a66b89a69168d5fbbd75ca390
wooseokoh <allsafe1206@gmail.com>
2021-09-25 16:16:05 +0900

    추가 : 로그인 유지 및 전역 상태 관리

--- community/pages/_app.js
+++ community/pages/_app.js
@@ -1,20 +1,45 @@
-import '../styles/globals.css'
 import Head from 'next/head';
+import Cookies from 'universal-cookie';
+import axios from 'axios';
+import '../styles/globals.css'
+import {useAtom} from "jotai";
+import authAtom from "../stores/authAtom";
+import {useEffect} from "react";
 
-function MyApp({ Component, pageProps }) {
-  return <>
-  <Head>
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
-        rel="stylesheet" />
-      <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU"
-        crossOrigin="anonymous" />
-  </Head>
-  
-  <Component {...pageProps} />
-  </>
+function MyApp({Component, pageProps}) {
+    const cookies = new Cookies();
+    const token = cookies.get('token');
+    const [, setAuth] = useAtom(authAtom);
+    if (token) {
+        axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+    }
+
+    useEffect(() => {
+        if (token) {
+            setAuth(auth => ({...auth, token}))
+            axios.get(`${process.env.API_HOST}/me`)
+                .then(response => setAuth(auth => ({...auth, user: response.data})))
+                .catch(() => {
+                })
+                .finally(() => setAuth(auth => ({...auth, loaded: true})))
+        } else {
+            setAuth(auth => ({...auth, loaded: true}))
+        }
+    }, [])
+
+    return <>
+        <Head>
+            <link
+                href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+                rel="stylesheet"/>
+            <link
+                href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
+                rel="stylesheet"
+                integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU"
+                crossOrigin="anonymous"/>
+        </Head>
+        <Component {...pageProps} />
+    </>
 }
 
 export default MyApp
